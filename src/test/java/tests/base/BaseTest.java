a3add00f4ead166abaff2c388fc5cadd85ab1d53
Serhii <s.shkurenko@treeum.net>
2022-05-12 14:39:18 +0300

    Add screenshots to Allure

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,64 +1,64 @@
-package tests.base;
-
-import common.CommonActions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import pages.base.BasePage;
-import pages.loans.CarLoansPage;
-import pages.telecomunications.MobilePhoneReplenishmentPage;
-
-import java.io.File;
-import java.time.LocalTime;
-import java.util.Objects;
-
-import static common.Config.*;
-
-@ExtendWith(Listener.class)
-//@Execution(ExecutionMode.CONCURRENT)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class BaseTest {
-    protected WebDriver driver = CommonActions.createDriver();
-    protected BasePage basePage = new BasePage(driver);
-    protected CarLoansPage carLoansPage = new CarLoansPage(driver);
-    protected MobilePhoneReplenishmentPage mobilePhoneReplenishmentPage = new MobilePhoneReplenishmentPage(driver);
-    private static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
-    /** Directory cleanup methods */
-    static{
-        LOGGER.info("START TIME:" + LocalTime.now());
-        LOGGER.info("Start clear reports dir: build/reports...");
-        File allureResult = new File("allure-results");
-        if (allureResult.isDirectory()) {
-            for(File item : Objects.requireNonNull(allureResult.listFiles()))
-            item.delete();
-        }
-
-        if (CLEAR_REPORTS_DIR) {
-            File allureScreenshots = new File("build/reports/tests");
-            for(File item : Objects.requireNonNull(allureScreenshots.listFiles()))
-                item.delete();
-        }
-    }
-
-    @AfterEach
-    void clearCookiesAndLocalStorage(){
-        if (CLEAR_COOKIES){
-            JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
-            driver.manage().deleteAllCookies();
-            javascriptExecutor.executeScript("window.sessionStorage.clear()");
-        }
-    }
-    @AfterAll
-    void close(){
-        if (!HOLD_BROWSER_OPEN){
-            driver.close();
-        }
-    }
-}
+//package tests.base;
+//
+//import common.CommonActions;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.TestInstance;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.jupiter.api.parallel.Execution;
+//import org.junit.jupiter.api.parallel.ExecutionMode;
+//import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.WebDriver;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import pages.base.BasePage;
+//import pages.loans.CarLoansPage;
+//import pages.telecomunications.MobilePhoneReplenishmentPage;
+//
+//import java.io.File;
+//import java.time.LocalTime;
+//import java.util.Objects;
+//
+//import static common.Config.*;
+//
+//@ExtendWith(Listener.class)
+////@Execution(ExecutionMode.CONCURRENT)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//public class BaseTest {
+//    protected WebDriver driver = CommonActions.createDriver();
+//    protected BasePage basePage = new BasePage(driver);
+//    protected CarLoansPage carLoansPage = new CarLoansPage(driver);
+//    protected MobilePhoneReplenishmentPage mobilePhoneReplenishmentPage = new MobilePhoneReplenishmentPage(driver);
+//    private static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
+//    /** Directory cleanup methods */
+//    static{
+//        LOGGER.info("START TIME:" + LocalTime.now());
+//        LOGGER.info("Start clear reports dir: build/reports...");
+//        File allureResult = new File("allure-results");
+//        if (allureResult.isDirectory()) {
+//            for(File item : Objects.requireNonNull(allureResult.listFiles()))
+//            item.delete();
+//        }
+//
+//        if (CLEAR_REPORTS_DIR) {
+//            File allureScreenshots = new File("build/reports/tests");
+//            for(File item : Objects.requireNonNull(allureScreenshots.listFiles()))
+//                item.delete();
+//        }
+//    }
+//
+//    @AfterEach
+//    void clearCookiesAndLocalStorage(){
+//        if (CLEAR_COOKIES){
+//            JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+//            driver.manage().deleteAllCookies();
+//            javascriptExecutor.executeScript("window.sessionStorage.clear()");
+//        }
+//    }
+//    @AfterAll
+//    void close(){
+//        if (!HOLD_BROWSER_OPEN){
+//            driver.close();
+//        }
+//    }
+//}
